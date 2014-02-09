5c48a186a0028bfcbfa8d1337d5b25abba961d4e
Doktor Kelly <doktorkelly@gmail.com>
2014-02-09 09:37:19 +0100

    initial creation

---
+++ chinook/chinook/DAL/ChinookContext.cs
@@ -0,0 +1,23 @@
+﻿using chinook.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace chinook.DAL
+{
+    public class ChinookContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>().ToTable("Artist", "public");
+            modelBuilder.Entity<Album>().ToTable("Album", "public");
+            modelBuilder.Conventions.Remove<StoreGeneratedIdentityKeyConvention>();
+        }
+    }
+}
---
+++ chinook/chinook/Models/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chinook.Models
+{
+    public class Artist
+    {
+        public Artist()
+        {
+            Albums = new List<Album>();
+        }
+
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
