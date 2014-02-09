896ebda70f96181d22936b4494d51e958d60836c
Doktor Kelly <doktorkelly@gmail.com>
2014-02-09 15:59:25 +0100

    setup db connection

---
+++ chinook/chinook/Controllers/ArtistController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using chinook.Models;
+using chinook.DAL;
+
+namespace chinook.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private ChinookContext db = new ChinookContext();
+
+        // GET: /Artist/
+        public ActionResult Index()
+        {
+            IList<Artist> model = db.Artists
+                .OrderBy(a => a.ArtistId)
+                .ToList();
+            return View(model);
+        }
+
+        // GET: /Artist/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // GET: /Artist/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Artist/Create
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ArtistId,Name")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Artists.Add(artist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(artist);
+        }
+
+        // GET: /Artist/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: /Artist/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ArtistId,Name")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+
+        // GET: /Artist/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: /Artist/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            db.Artists.Remove(artist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
